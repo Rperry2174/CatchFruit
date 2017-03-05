5c254e9e59f92e5720f5ad53f4d1a68bb3b6294c
Ryan Perry <Rperry2174>
2017-03-05 18:14:36 -0500

    Updated art for kanye and fruit, removed old sprite, eliminated extra code

--- boardSetup/js/main.js
+++ boardSetup/js/main.js
@@ -1,15 +1,15 @@
 
 var tween;
-var kirby;
 var kanye;
 var tween1;
 var tween2;
 var cursors;
 var redFruit;
+var explosions;
 var goldFruit;
-var SpikeFruit;
+var BombFruit;
 var fruitsGroup;
-var kirbyScoreText;
+var kanyeScoreText;
 var fallingTimer = 0;
 
 
@@ -19,20 +19,11 @@ IndividualFruit = function(game, x, y, name){
   this.anchor.setTo(0.5,0);
   this.hasBounced = false;
 
-  //when checkWorldBounds is true this.events.onOutOfBounds is enabled
-  // this.checkWorldBounds = true;
-  // this.events.onOutOfBounds.add(fruitOut, this);
-
   //adds this existing sprite to gameboard
   game.add.existing(this);
 
-  //makes (currently) GLOBAL gravity effect all fruits
+  //allows (currently) GLOBAL gravity effect all fruits
   game.physics.enable(this);
-  // var tween = game.add.tween(this).to({alpha: 0}, 1000, Phaser.Easing.Linear.None, false);
-  // tween.onComplete.add(function(fruit){
-  //           fruit.kill()
-  //           fruit.alpha = 100;
-  //         });
 
 };
 
@@ -77,27 +68,25 @@ GoldFruit = function(game, x, y, name){
   this.value = 5;
   this.speedMultiplier = 2;
   console.log("GoldFruit", this)
-
 };
 
 GoldFruit.prototype = Object.create(IndividualFruit.prototype);
 GoldFruit.prototype.constructor = GoldFruit;
 
 //////////////////////////////////////////////
-//Sprite ==> Individual Fruit ==> SpikeFruit//
+//Sprite ==> Individual Fruit ==> BombFruit//
 //////////////////////////////////////////////
 
-SpikeFruit = function(game, x, y, name){
+BombFruit = function(game, x, y, name){
   IndividualFruit.call(this, game, x, y, name);
 
   this.value = -5;
   this.speedMultiplier = 0.5;
-  console.log("SpikeFruit", this)
-
+  console.log("BombFruit", this)
 };
 
-SpikeFruit.prototype = Object.create(IndividualFruit.prototype);
-SpikeFruit.prototype.constructor = SpikeFruit;
+BombFruit.prototype = Object.create(IndividualFruit.prototype);
+BombFruit.prototype.constructor = BombFruit;
 
 
 Player = function(game, x, y, name){
@@ -116,13 +105,12 @@ Player = function(game, x, y, name){
   this.maxSpeed = 300;
 
   this.physicsBodyType = Phaser.Physics.ARCADE;
-}
+};
+
 Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player;
 
-
 Player.prototype.setSpeed = function(multiplier){
-
   //needs max speed property
   this.speed *= multiplier;
   this.game.time.events.add(Phaser.Timer.SECOND * 2, function(){
@@ -130,21 +118,20 @@ Player.prototype.setSpeed = function(multiplier){
   }, this);
 };
 
-
 Fruits = function (game) {
   Phaser.Group.call(this, game);
   this.enableBody = true;
   this.physicsBodyType = Phaser.Physics.ARCADE;
 
   for (var x = 0; x < 3; x++){
-      var fruit1 = new RedFruit(game, x*100, 10, 'redFruit');
-      var fruit2 = new GoldFruit(game, x*150, 10, 'goldFruit');
-      var fruit3 = new SpikeFruit(game, x*300, 10, 'spikeFruit');
+    var fruit1 = new RedFruit(game, x*100, 10, 'redFruit');
+    var fruit2 = new GoldFruit(game, x*150, 10, 'goldFruit');
+    var fruit3 = new BombFruit(game, x*300, 10, 'bombFruit');
 
-      this.add(fruit1);
-      this.add(fruit2);
-      this.add(fruit3);
-    }
+    this.add(fruit1);
+    this.add(fruit2);
+    this.add(fruit3);
+  };
 
   this.setAll('checkWorldBounds', true);
   this.setAll('body.collideWorldBounds', true);
@@ -154,75 +141,45 @@ Fruits = function (game) {
 Fruits.prototype = Object.create(Phaser.Group.prototype);
 Fruits.prototype.constructor = Fruits;
 
-
-
 var GameState = {
   preload: function(){
     game.load.image('background', 'assets/forrest_background.png');
-    game.load.image('kirby', 'assets/kirby.png');
     game.load.image('redFruit', 'assets/apple.png')
     game.load.image('goldFruit', 'assets/goldApple.png')
-    game.load.image('watermellon', 'assets/watermellon.png')
-    game.load.image('spikeFruit', 'assets/spikeBall.png')
+    game.load.image('bombFruit', 'assets/bombFruit.png')
     game.load.spritesheet('scott', 'assets/scottpilgrim_test864x280.png', 864 / 8 , 280 / 2, 16)
     game.load.spritesheet('kaboom', 'assets/explode.png', 128, 128);
-
     game.load.spritesheet('kanye', 'assets/bear_animation_test.png',870/10,166, 20,1,1);
-
   },
 
   create:function(){
     //setup game background, physics, and gravity
     game.background = game.add.sprite(0, 0, 'background');
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    // game.physics.arcade.gravity.y = 100;
-
-
-
-
-    kirby = new Player(game, game.world.centerX, 350, 'kirby');
-    kirby.scale.setTo(0.25, 0.25);
-    console.log('kirby', kirby);
-    //setup score texts
-    kirbyScoreText = game.add.text(kirby.body.x - 25, kirby.body.y -60, 'Kirby: 0', { fontSize: '14px', fill: '#000' });
 
     kanye = new Player(game, 500, 350, 'kanye');
+    kanyeScoreText = game.add.text(16, 16, 'Kanye: 0', { fontSize: '14px', fill: '#000' });
 
-    //to be added to the players prototype next
-    // var walkright = kanye.animations.add('walkright', [11, 12, 13, 14, 15, 16, 17, 18], 10 , true);
     var walkleft = kanye.animations.add('walkleft', [0, 1, 2, 3, 4, 5, 6, 7] , 10, true);
-    var stand = kanye.animations.add('stand', [9] , 1, true);
+    //var stand = kanye.animations.add('stand', [9, 10, 11, 12, 13, 14, 15, 16] , 10, true);
+    var stand = kanye.animations.add('stand', [15] , 1, true);
 
-    fruitsGroup = new Fruits(game); //all code below condensed into ^
-
-    // redFruit = new RedFruit(game, 100, 10, 'redFruit');
-    // goldFruit = new GoldFruit(game, 200, 10, 'goldFruit');
-    // spikeFruit = new SpikeFruit(game, 300, 10, 'spikeFruit');
-
-    // fruitsGroup = game.add.group();
-    // fruitsGroup.add(redFruit);
-    // fruitsGroup.add(goldFruit);
-    // fruitsGroup.add(spikeFruit);
-    // fruitsGroup.setAll('body.collideWorldBounds', true);
-    // fruitsGroup.setAll('body.bounce.y', 0.2);
-
+    //setup fruits group
+    fruitsGroup = new Fruits(game);
     console.log("fruitsGroup", fruitsGroup);
 
-    //set up cursor responses
-    cursors = game.input.keyboard.createCursorKeys(); //arrow keys
-
-
-        //  An explosion pool
+    //  An explosion pool
     explosions = game.add.group();
     explosions.createMultiple(3, 'kaboom');
     explosions.forEach(setupFruitExplosion, this);
 
+    //set up cursor responses
+    cursors = game.input.keyboard.createCursorKeys(); //arrow keys
 
   },
 
   update:function(){
 
-    game.physics.arcade.overlap(fruitsGroup, kirby, fruitCatchHandler, fruitNoBounced, this)
     game.physics.arcade.overlap(fruitsGroup, kanye, fruitCatchHandler, fruitNoBounced, this)
 
     fruitsGroup.forEach(function(fruit){
@@ -241,32 +198,28 @@ var GameState = {
     // var bouncePhaseEnd = furit.body.onFloor() && furit.hasBounced;
 
     if (game.time.now > fallingTimer){
-      // console.log("game.time.now", game.time.now);
-      // console.log("fallingTimer", fallingTimer)
       fruitFalls();
       reviveFruit();
       fallingTimer += 2000;
-    }
-
+    };
     kanye.body.velocity.x = 0;
+
     if(cursors.left.isDown){
       kanye.scale.x = 1;
-
       kanye.animations.play('walkleft');
       kanye.body.velocity.x = -kanye.speed;
-
     };
+
     if(cursors.right.isDown){
-      kanye.body.velocity.x = kanye.speed;
       kanye.scale.x = -1;
       kanye.animations.play('walkleft');
+      kanye.body.velocity.x = kanye.speed;
     };
 
     if(!cursors.right.isDown && !cursors.left.isDown){
       kanye.animations.play('stand');
-    }
+    };
 
-    kirbyScoreText.position.x = kirby.body.x + 17;
   }
 
 };
@@ -296,16 +249,17 @@ function reviveFruit() {
 function fruitCatchHandler(player, fruit){
   console.log("player in collision", player);
 
-  if(fruit.value === -5){
+  if(fruit.key === "bombFruit"){
     var explosion = explosions.getFirstExists(false);
     explosion.reset(fruit.body.x, fruit.body.y);
-    explosion.play('kaboom', 30, false, true);  console.log("fruit in collision", fruit);
+    explosion.play('kaboom', 30, false, true);
+    console.log("fruit in collision", fruit);
   }
 
   console.log("fruit.value in collision", fruit.value);
   player.score += fruit.value;
   player.setSpeed(fruit.speedMultiplier);
-  kirbyScoreText.text = 'Kirby: ' + kirby.score;
+  kanyeScoreText.text = 'Kanye: ' + kanye.score;
   fruit.kill();
 }
 
